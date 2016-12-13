ae7f8e59bdfd254cfc8c3427f5c7572a7b7b8437
yexiaozhu <557703988@139.com>
2016-12-12 23:43:04 -0800

    Chapter 01: finding service name

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -3,14 +3,12 @@
 #author="yexiaozhu"
 
 import socket
-from binascii import hexlify
 
+def find_service_name():
+    protocolname = 'tcp'
+    for port in [80, 25]:
+        print("Port: %s => service name: %s" %(port, socket.getservbyport(port, protocolname)))
+    print("Port: %s => service name: %s" %(53, socket.getservbyport(53, 'udp')))
 
-def convert_ip4_address():
-    for ip_addr in ['127.0.0.1', '192.168.0.1']:
-        packed_ip_addr = socket.inet_aton(ip_addr)
-        unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
-        print("IP Address: %s => Packed: %s, Unpacked: %s"
-              %(ip_addr, hexlify(packed_ip_addr), unpacked_ip_addr))
 if __name__ == '__main__':
-    convert_ip4_address()
+    find_service_name()
