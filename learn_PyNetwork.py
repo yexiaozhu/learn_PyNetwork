f0f7236eaac157ba2063f342fb2c19e45def17e4
yexiaozhu <557703988@139.com>
2016-12-12 23:32:08 -0800

    Chapter 01: ip4 address conversion

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -3,11 +3,14 @@
 #author="yexiaozhu"
 
 import socket
-def get_remote_machine_info():
-    remote_host = 'www.python.org'
-    try:
-        print("IP address of %s: %s" %(remote_host, socket.gethostbyname(remote_host)))
-    except socket.error as err_msg:
-        print("%s: %s" %(remote_host, err_msg))
+from binascii import hexlify
+
+
+def convert_ip4_address():
+    for ip_addr in ['127.0.0.1', '192.168.0.1']:
+        packed_ip_addr = socket.inet_aton(ip_addr)
+        unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
+        print("IP Address: %s => Packed: %s, Unpacked: %s"
+              %(ip_addr, hexlify(packed_ip_addr), unpacked_ip_addr))
 if __name__ == '__main__':
-    get_remote_machine_info()
+    convert_ip4_address()
