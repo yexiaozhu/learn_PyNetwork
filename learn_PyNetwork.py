5b8ea03ef9841d70bc7c7cbbf3c9775ad6d9dd3a
yexiaozhu <557703988@139.com>
2016-12-13 19:23:15 -0800

    Chapter 01: sntp client

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 #coding=utf-8
 #author="yexiaozhu"
-import ntplib
-from time import ctime
+import socket
+import struct
 
-def print_time():
-    ntp_client = ntplib.NTPClient()
-    response = ntp_client.request('europe.pool.ntp.org', version=3)
-    print(ctime(response.tx_time))
+import time
+
+NTP_SERVER = "0.uk.pool.ntp.org"
+TIME1970 = 2208988800
+
+def sntp_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = '\x1b' + 47 * '\0'
+    client.sendto(data.encode(encoding='utf-8'), (NTP_SERVER, 123))
+    data, address = client.recvfrom(1024)
+    if data:
+        print('Response received from:', address)
+    t = struct.unpack('!12I', data)[10]
+    t -= TIME1970
+    print('\tTime=%s' %time.ctime(t))
 
 if __name__ == '__main__':
-    print_time()
+    sntp_client()
