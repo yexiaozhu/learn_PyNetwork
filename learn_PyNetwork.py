80d91481caee873b50fdd2b7f0276af9dfed9cff
yexiaozhu <557703988@139.com>
2017-01-21 00:00:06 -0800

    Chapter 03: ping remote host(python2 成功 3失败)

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -1,102 +1,143 @@
 #!/usr/bin/env python3
-#coding=utf-8
+#-*- coding=utf-8 -*-
 #author="yexiaozhu"
+
+import os
 import argparse
-
-LOCAL_SERVER_HOST = 'localhost'
-REMOTE_SERVER_HOST = 'www.baidu.com'
-BUFSIZE = 4096
-
-import asyncore
 import socket
+import struct
+import select
+import time
+import sys
 
+ICMP_ECHO_REQUEST = 8 # Platform specific
+DEFAULT_TIMEOUT = 2
+DEFAULT_COUNT = 4
 
-class PortForwarder(asyncore.dispatcher):
-    def __init__(self, ip, port, remoteip, remoteport, backlog=5):
-        asyncore.dispatcher.__init__(self)
-        self.remoteip = remoteip
-        self.remoteport = remoteport
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((ip, port))
-        self.listen(backlog)
+class Pinger(object):
+    """ Pings to a host -- the Pythonic way """
 
-    def handle_accept(self):
-        conn, addr = self.accept()
-        print("Connected to: ", addr)
-        # print('Receiver(conn)=', Receiver(conn))
-        # print('self.remoteip=', self.remoteip, 'self.remoteport=', self.remoteport)
-        #print('conn=', conn)
-        Sender(Receiver(conn), self.remoteip, self.remoteport)
+    def __init__(self, target_host, count=DEFAULT_COUNT, timeout=DEFAULT_TIMEOUT):
+        self.target_host = target_host
+        self.count = count
+        self.timeout = timeout
 
-class Receiver(asyncore.dispatcher):
-    def __init__(self, conn):
-        asyncore.dispatcher.__init__(self, conn)
-        self.from_remote_buffer = ''
-        self.to_remote_buffer = ''
-        self.sender = None
+    def do_checksum(self, source_string):
+        """ Verify the packet integritity"""
+        sum = 0
+        #print('source_string=', source_string)
+        #print('source_string=', type(source_string))
+        source_string = source_string.decode('utf-16')
+        max_count = (len(source_string)/2)*2
+        count = 0
+        while count < max_count:
+            val = ord(source_string[count + 1])*256 + ord(source_string[count]) # ord()返回字符串对应当ascii码
+            sum = sum + val
+            sum = sum & 0xffffffff # & 位运算 全部为1，结果为1
+            count = count + 2
 
-    def handle_connect(self):
-        pass
+        if max_count<len(source_string):
+            sum = sum + ord(source_string[len(source_string) - 1])
+            sum = sum & 0xffffffff
 
-    def handle_read(self):
-        read = self.recv(BUFSIZE)
-        #print('read',type(read))
-        #print('read=',read.decode(encoding='utf-8'))
-        #print(type(self.from_remote_buffer))
-        self.from_remote_buffer += read.decode(encoding='utf-8')
-        #print('self.from_remote_buffer=', self.from_remote_buffer)
+        sum = (sum >> 16) + (sum & 0xffff)
+        sum = sum + (sum >> 16) # >>右移运算符 把>>左边运算数二进制全部右移若干位
+        answer = ~sum # ～按位取反 对数据二进制取反 0变1 1变0
+        answer = answer & 0xffff
+        answer = answer >> 8 | (answer << 8 & 0xff00)
+        return answer
 
-    def writeable(self):
-        return (len(self.to_remote_buffer) > 0)
+    def receive_ping(self, sock, ID, timeout):
+        """ Receive ping from the socket."""
+        time_remaining = timeout
+        while True:
+            start_time = time.time()
+            readable = select.select([sock], [], [], time_remaining)
+            time_spent = (time.time() - start_time)
+            if readable[0] == []: # Timeout
+                return
+            time_received = time.time()
+            recv_packet, addr = sock.recvfrom(1024)
+            icmp_header = recv_packet[20:28]
+            type, code, checksum, pack_ID, sequence = struct.unpack('bbHHh', icmp_header)
+            print(type, code, checksum, pack_ID, sequence)
+            if pack_ID == ID:
+                bytes_In_double = struct.calcsize("d")
+                time_sent = struct.unpack("d", recv_packet[28:28 + bytes_In_double])[0]
+                return time_received - time_spent
 
-    def handle_write(self):
-        sent = self.send(self.to_remote_buffer.encode(encoding='utf-8'))
-        #print('sent2=', sent)
-        self.to_remote_buffer = self.to_remote_buffer[sent:]
-        #print('self.to_remote_buffer=', self.to_remote_buffer)
+            time_remaining = time_remaining - time_spent
+            if time_remaining <= 0:
+                return
 
-    def handle_close(self):
-        self.close()
-        if self.sender:
-            self.sender.close()
+    def send_ping(self, sock, ID):
+        """ Send ping to the target host"""
+        target_addr = socket.gethostbyname(self.target_host)
 
-class Sender(asyncore.dispatcher):
-    def __init__(self, receiver, remoteaddr, remoteport):
-        asyncore.dispatcher.__init__(self)
-        self.receiver = receiver
-        receiver.sender = self
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect((remoteaddr, remoteport))
+        my_checksum = 0
 
-    def handle_connect(self):
-        pass
+        # Create a dummy heder with a 0 checksum.
+        #print(ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
+        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
+        #print('header=', header)
+        bytes_In_double = struct.calcsize('d')
+        #print(bytes_In_double)
+        data = (192 - bytes_In_double) * "Q"
+        #print(type(data))
+        #print('data=', data)
+        #print('data1=', struct.pack("d", time.time()))
+        data = struct.pack("d", time.time()) + data.encode(encoding='utf-8')
+        #print('data2=', data)
 
-    def handle_read(self):
-        read = self.recv(BUFSIZE)
-        self.receiver.to_remote_buffer += read.decode(encoding='utf-8')
+        # Get the checksum
+        my_checksum = self.do_checksum(header + data)
+        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1)
+        packet = header + data
+        sock.sendto(packet, (target_addr, 1))
 
-    def writable(self):
-        return (len(self.receiver.from_remote_buffer) > 0)
+    def ping_once(self):
+        """ Returns the delay (in seconds) or none on timeout."""
+        icmp = socket.getprotobyname("icmp")
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+        except socket.error as e:
+            (errno, msg) = e
+            if errno == 1:
+                # Not superuser, so operation not permitted
+                msg += "ICMP messages can only be sent from root user processes"
+                raise socket.error(msg)
+        except Exception as e:
+            print("Execption: %s" %(e))
 
-    def handle_write(self):
-        sent = self.send(self.receiver.from_remote_buffer.encode(encoding='utf=8'))
-        self.receiver.from_remote_buffer = self.receiver.from_remote_buffer[sent:]
+        my_ID = os.getpid() & 0xFFFF
+        print(sock, my_ID)
+        self.send_ping(sock, my_ID)
+        delay = self.receive_ping(sock, my_ID, self.timeout)
+        sock.close()
+        #print(delay)
+        return delay
 
-    def handle_close(self):
-        self.close()
-        self.receiver.close()
+    def ping(self):
+        """ Run the ping process"""
+        for i in range(self.count):
+            print("Ping to %s..." %self.target_host)
+            try:
+                delay = self.ping_once()
+            except socket.gaierror as e:
+                print("Pin failed. (socket error: '%s')" %e[1])
+                break
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Stackless Socket Server Example')
-    parser.add_argument('--local-host', action="store", dest="local_host", default=LOCAL_SERVER_HOST)
-    parser.add_argument('--local-port', action="store", dest="local_port", type=int, required=True)
-    parser.add_argument('--remote-host', action="store", dest="remote_host", default=REMOTE_SERVER_HOST)
-    parser.add_argument('--remote-port', action="store", dest="remote_port", type=int, default=80)
+            if delay == None:
+                print("Ping failed. (timeout within % ssec.)" %self.timeout)
+            else:
+                delay = delay * 1000
+                print("Get ping in %0.4fms" %delay)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Python ping')
+    parser.add_argument('--target-host', action="store", dest="target_host",
+    required=True)
     given_args = parser.parse_args()
-    local_host, remote_host = given_args.local_host, given_args.remote_host
-    local_port, remote_port = given_args.local_port, given_args.remote_port
-    print("Starting port forwarding local %s:%s => remote %s:%s"
-          % (local_host, local_port, remote_host, remote_port))
-    PortForwarder(local_host, local_port, remote_host, remote_port)
-    asyncore.loop()
+    target_host = given_args.target_host
+    pinger = Pinger(target_host=target_host)
+    pinger.ping()
