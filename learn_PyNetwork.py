6d9b23ef8c1fcc25e5462063325c32be385539b7
yexiaozhu <557703988@139.com>
2017-02-06 00:33:09 -0800

    Chapter 03: get interface ip address

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,42 +2,24 @@
 #-*- coding=utf-8 -*-
 #author="yexiaozhu"
 
+import argparse
 import sys
 import socket
 import fcntl
 import struct
 import array
 
-SIOCGIFCONF = 0x8912 # from C library sockios.h
-STUCT_SIZE_32 = 32
-STUCT_SIZE_64 = 40
-PLATFORM_32_MAX_NUMBER = 2**32
-DEFAULT_INTERFACES = 8
-
-def list_interfaces():
-    interfaces = []
-    max_interfacers = DEFAULT_INTERFACES
-    is_64bits = sys.maxsize > PLATFORM_32_MAX_NUMBER
-    struct_size = STUCT_SIZE_64 if is_64bits else STUCT_SIZE_32
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    while True:
-        bytes = max_interfacers * struct_size
-        interface_names = array.array('B', '\0' * bytes)
-        sock_info = fcntl.ioctl(
-            sock.fileno(),
-            SIOCGIFCONF,
-            struct.pack('iL', bytes, interface_names.buffer_info()[0])
-        )
-        outbytes = struct.unpack('iL', sock_info)[0]
-        if outbytes == bytes:
-            max_interfacers *= 2
-        else:
-            break
-    namestr = interface_names.tostring()
-    for i in range(0, outbytes, struct_size):
-        interfaces.append((namestr[i:i+16].split('\0', 1)[0]))
-    return interfaces
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915, # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 if __name__ == '__main__':
-    interfaces = list_interfaces()
-    print "This machine has %s network interfaces: %s." %(len(interfaces), interfaces)
+    parser = argparse.ArgumentParser(description='Python networking utils')
+    parser.add_argument('--ifname', action="store", dest="ifname", required=True)
+    given_args = parser.parse_args()
+    ifname = given_args.ifname
+    print "Interface [%s] ==> IP: %s" %(ifname, get_ip_address(ifname))
