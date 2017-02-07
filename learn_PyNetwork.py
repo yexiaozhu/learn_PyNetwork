2165d44773df4c48b73a479b2a900b781c3a5da8
yexiaozhu <557703988@139.com>
2017-02-07 01:59:09 -0800

    Chapter 03: find network interface status

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -3,23 +3,27 @@
 #author="yexiaozhu"
 
 import argparse
-import sys
 import socket
 import fcntl
 import struct
-import array
+import nmap
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915, # SIOCGIFADDR
+SAMPLE_PORTS = '21-23'
+
+def get_interface_status(ifname):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip_address = socket.inet_ntoa(fcntl.ioctl(
+        sock.fileno(),
+        0x8915, #SIOCGIFADDR, C socket library sockios.h
         struct.pack('256s', ifname[:15])
     )[20:24])
+    nm = nmap.PortScanner()
+    nm.scan(ip_address, SAMPLE_PORTS)
+    return nm[ip_address].state()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Python networking utils')
     parser.add_argument('--ifname', action="store", dest="ifname", required=True)
     given_args = parser.parse_args()
     ifname = given_args.ifname
-    print "Interface [%s] ==> IP: %s" %(ifname, get_ip_address(ifname))
+    print "Interface [%s] is: %s" % (ifname, get_interface_status(ifname))
