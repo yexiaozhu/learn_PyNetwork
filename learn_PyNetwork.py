992dc8bd64b7868e9c4e8a314d10807ae78772d9
557703988@139.com <557703988@139.com>
2017-03-06 21:58:04 -0800

    Chapter 03: extract ipv6 prefix

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -5,32 +5,30 @@
 import socket
 import argparse
 import netifaces as ni
+import netaddr as na
 
-def inspect_ipv6_support():
-    """ Find the ipv6 address """
+def extract_ipv6_info():
+    """ Extracts IPv6 information"""
     print "IPV6 support built into Python: %s" %socket.has_ipv6
-
-    ipv6_addr = {}
     for interface in ni.interfaces():
         all_addresses = ni.ifaddresses(interface)
         print "Interface %s:" %interface
-        for family, addrs in all_addresses.iteritems():
+        for family,addrs in all_addresses.iteritems():
             fam_name = ni.address_families[family]
             print 'Address family: %s' %fam_name
             for addr in addrs:
                 if fam_name == 'AF_INET6':
-                    ipv6_addr[interface] = addr['addr']
-                print 'Address : %s' %addr['addr']
-                nmask = addr.get('netmask', None)
-                if nmask:
-                    print 'Netmask : %s' %nmask
-                bcast = addr.get('broadcast', None)
-                if bcast:
-                    print 'Broadcast: %s' %bcast
-    if ipv6_addr:
-        print "Found IPv6 address: %s" %ipv6_addr
-    else:
-        print "No IPv6 interface found!"
+                    addr = addr['addr']
+                    has_eth_string = addr.split("%eth")
+                    if has_eth_string:
+                        addr = addr.split("%ens")[0]
+                        # addr = addr.split("%eth")[0]
+                        # print '1', addr
+                    print "    IP Address: %s" %na.IPNetwork(addr)
+                    print "    IP Version: %s" %na.IPNetwork(addr).version
+                    print "    IP Prefix length: %s" %na.IPNetwork(addr).prefixlen
+                    print "    Network: %s" %na.IPNetwork(addr).network
+                    print "    Broadcast: %s" %na.IPNetwork(addr).broadcast
 
 if __name__ == '__main__':
-    inspect_ipv6_support()
+    extract_ipv6_info()
