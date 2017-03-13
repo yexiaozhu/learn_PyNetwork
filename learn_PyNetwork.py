6f360a97bdd969d07e023ed1481bfba445421d83
557703988@139.com <557703988@139.com>
2017-03-12 23:53:57 -0700

    Chapter 04: submit web form

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,47 +2,35 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import cookielib
+import requests
 import urllib
 import urllib2
 
-ID_USERNAME = 'id_username'
-ID_PASSWORD = 'id_password'
-USERNAME = 'XXXXXX'
+ID_MOBILEPHONE = 'id_mobilePhone'
+ID_EMAIL = 'id_Email'
+ID_PASSWORD = 'id_Password'
+ID_RELOAD_PASSWORD = 'id_reload_Password'
+MOBILEPHONE = 'XXXXXX'
+# USERNAME = 'XXXXXX'
+EMAIL = 'XXXXXX'
 PASSWORD = 'XXXXXX'
-LOGIN_URL = 'https://github.com/login'
-NORMAL_URL = 'https://github.com/'
-headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'
-        }
-def extract_cookie_info():
-    """ Fake login to a site with cookie """
-    # setuo cookie jar
-    cj = cookielib.CookieJar()
-    # print cj
-    login_data = urllib.urlencode({ID_USERNAME: USERNAME,
-                                   ID_PASSWORD: PASSWORD})
-    # create url opener
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    # opener.addheaders = headers
-    # urllib2.install_opener(opener)
-    # opener = urllib2.build_opener(headers)
-    # urllib2.install_opener(opener)
-    # resp = opener.open(LOGIN_URL, login_data)
-    resp = urllib2.Request(LOGIN_URL, login_data, headers)
+SIGUP_URL = 'https://accounts.ctrip.com/member/regist/register.aspx'
 
-    # send login info
-    for cookie in cj:
-        print "----First time cookie: %s --> %s" %(cookie.name, cookie.value)
+def submit_form():
+    """ Submit a from """
+    payload = {ID_MOBILEPHONE : MOBILEPHONE,
+               ID_EMAIL : EMAIL,
+               ID_PASSWORD : PASSWORD,
+               ID_RELOAD_PASSWORD : PASSWORD}
 
-    print "Headers: %s" %resp.headers
+    # make a get request
+    resp = requests.get(SIGUP_URL)
+    print "Response to GET request: %s" %resp.content
 
-    # now acess without any login info
-    resp = opener.open(NORMAL_URL)
-    for cookie in cj:
-        print "++++Second time cookie: %s --> %s" %(cookie.name, cookie.value)
-
-    print "Header: %s" %resp.headers
+    # send POST request
+    resp = requests.post(SIGUP_URL, payload)
+    print "Headers from a POST request response: %s" %resp.headers
+    # print "HTML Response: %s" %resp.read()
 
 if __name__ == '__main__':
-    extract_cookie_info()
+    submit_form()
