4581d833e9071e4358f17438a53f15e3d4df92ce
557703988@139.com <557703988@139.com>
2017-03-13 23:45:39 -0700

    Chapter 04: checking webpage with HEAD request

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,11 +2,32 @@
 #coding=utf-8
 #author="yexiaozhu"
 
+import argparse
+import httplib
+import urlparse
+import re
 import urllib
 
-URL = 'https://www.github.com'
-PROXY_ADDRESS = "165.24.10.8:8080" # BY Googling free proxy server
+DEFAULT_URL = 'http://www.python.org'
+HTTP_GOOD_CODES = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
+
+def get_server_status_code(url):
+    """ Download just the header of a URL and return the server's status code. """
+    host, path = urlparse.urlparse(url)[1:3]
+    try:
+        conn = httplib.HTTPConnection(host)
+        conn.request('HEAD', path)
+        return conn.getresponse().status
+    except StandardError:
+        return None
 
 if __name__ == '__main__':
-    resp = urllib.urlopen(URL, proxies= {"http" : PROXY_ADDRESS})
-    print "Proxy server returns response headers: %s" %resp.headers
+    parser = argparse.ArgumentParser(description='Example HEAD Request')
+    parser.add_argument('--url', action="store", dest="url", default=DEFAULT_URL)
+    given_args = parser.parse_args()
+    url = given_args.url
+    if get_server_status_code(url) in HTTP_GOOD_CODES:
+        # print get_server_status_code(url)
+        print "Server: %s status is OK: " %url
+    else:
+        print "Server: %s status is NOT OK! " %url
