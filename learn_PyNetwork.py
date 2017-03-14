408eae07af9aa4d17cb280bbef88c429562e2e28
557703988@139.com <557703988@139.com>
2017-03-14 00:20:10 -0700

    Chapter 04: http compression

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,18 +2,51 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import urllib2
+import argparse
+import string
+import os
+import sys
+import gzip
+import cStringIO
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-BROWSER = 'Mozilla/5.0 (Windows NT 5.1; rv:20.0) Gecko/20100101 Firefox/20.0'
-URL = 'http://www.python.org'
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 8800
+HTML_CONTENT = """<html><body><h1>Compressed Hello World!</h1></body></html>"""
 
-def spoof_firefox():
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', BROWSER)]
-    result = opener.open(URL)
-    print "Response headers:"
-    for header in result.headers.headers:
-        print "\t", header
+class RequestHandler(BaseHTTPRequestHandler):
+    """ Custom request handler """
+
+    def do_GET(self):
+        """ Handler for the GET requests """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-Encoding', 'gzip')
+        zbuf = self.compress_buffer(HTML_CONTENT)
+        sys.stdout.write("Content-Encoding: gzip\r\n")
+        self.send_header('Content-Length', len(zbuf))
+        self.end_headers()
+
+        # Send the message to browser
+        zbuf = self.compress_buffer(HTML_CONTENT)
+        sys.stdout.write("Content-Encoding: gzip\r\n")
+        sys.stdout.write("Content-Length: %d\r\n" % (len(zbuf)))
+        sys.stdout.write("\r\n")
+        self.wfile.write(zbuf)
+        return
+
+    def compress_buffer(self, buf):
+        zbuf = cStringIO.StringIO()
+        zfile = gzip.GzipFile(mode = 'wb', fileobj = zbuf, compresslevel= 6)
+        zfile.write(buf)
+        zfile.close()
+        return zbuf.getvalue()
 
 if __name__ == '__main__':
-    spoof_firefox()
+    parser = argparse.ArgumentParser(description='Simple HTTP Server EXample')
+    parser.add_argument('--port', action="store", dest="port", type=int, default=DEFAULT_PORT)
+    given_args = parser.parse_args()
+    port = given_args.port
+    server_address = (DEFAULT_HOST, port)
+    server = HTTPServer(server_address, RequestHandler)
+    server.serve_forever()
