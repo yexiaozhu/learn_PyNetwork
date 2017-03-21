cfcffa0592d63c62b518a59d1a5dd7198a9809f0
557703988@139.com <557703988@139.com>
2017-03-20 23:36:26 -0700

    Chapter 05: list files on ftp server

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,35 +2,24 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import socket, os
-from SocketServer import BaseServer
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from OpenSSL import SSL
+FTP_SERVER_URL = 'ftp.debian.org'
+# FTP_SERVER_URL = 'ftp.kernel.org'
 
-class SecureHTTPServer(HTTPServer):
-    def __init__(self, server_address, HandlerClass):
-        BaseServer.__init__(self, server_address, HandlerClass)
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        fpem = 'server.pem' # location of the server private key and the server certificate
-        ctx.use_privatekey_file (fpem)
-        ctx.use_certificate_file(fpem)
-        self.socket = SSL.Connection(ctx, socket.socket(self.address_family, self.socket_type))
-        self.server_bind()
-        self.server_activate()
+import ftplib
 
-class SecureHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def setup(self):
-        self.connection = self.request
-        self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
-        self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
+def test_ftp_connection(path, username, email):
+    # Open ftp connection
+    ftp = ftplib.FTP(path, username, email)
 
-def run_server(HandlerClass = SecureHTTPRequestHandler, ServerClass = SecureHTTPServer):
-    server_address = ('', 4443) # port needs to be accessible by user
-    server = ServerClass(server_address, HandlerClass)
-    running_address = server.socket.getsockname()
-    print "Serving HTTPS Server on %s:%s ..." %(running_address[0], running_address[1])
-    server.serve_forever()
+    # List the files in the /pub directory
+    # ftp.cwd("debian")
+    ftp.cwd("debian/tools")
+    # ftp.cwd("kernel/pub")
+    print "File list at %s:" %path
+    files = ftp.dir()
+    print files
+
+    ftp.quit()
 
 if __name__ == '__main__':
-    run_server()
+    test_ftp_connection(path=FTP_SERVER_URL, username='anonymous', email='nobody@nourl.com')
