56077916594f15b9bb7126721b4399cf84f8e70c
557703988@139.com <557703988@139.com>
2017-03-24 02:14:35 -0700

    Chapter 05: email cuurent dir zipped

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -4,30 +4,51 @@
 
 import os
 import argparse
-import ftplib
-import getpass
+import smtplib
+import zipfile
+import tempfile
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
-LOCAL_FTP_SERVER = 'localhost'
-LOCAL_FILE = 'README.md'
+print os.getcwd()
+def email_dir_zipped(sender, recipient):
+    zf = tempfile.TemporaryFile(prefix='mail', suffix='.zip')
+    zip = zipfile.ZipFile(zf, 'w')
+    print "Zipping current dir: %s" %os.getcwd()
+    for file_name in os.listdir(os.getcwd()):
+        zip.write(file_name)
+    zip.close()
+    zf.seek(0)
 
-def ftp_upload(ftp_server, username, password, file_name):
-    print "Connecting to FTP server: %s" %ftp_server
-    ftp = ftplib.FTP(ftp_server)
-    print "Login to FTP server: user=%s" %username
-    ftp.login(username, password)
-    ext = os.path.splitext(file_name)[1]
-    if ext in (".txt", ".htm", ".html"):
-        ftp.storlines("STOR " + file_name, open(file_name))
-    else:
-        ftp.storbinary("STOR " +file_name, open(file_name, "rb"), 1024)
-    print "Uploaded file: %s" %file_name
+    # Create the message
+    print "Creating email message..."
+    email_msg = MIMEMultipart()
+    email_msg['Subject'] = 'File from path %s' %os.getcwd()
+    email_msg['To'] = ', '.join(recipient)
+    email_msg['From'] = sender
+    email_msg.preamble = 'Testing email from Python.\n'
+    msg = MIMEBase('application', 'zip')
+    msg.set_payload(zf.read())
+    encoders.encode_base64(msg)
+    msg.add_header('Content-Disposition', 'attachment', filename=os.getcwd()[-1] + '.zip')
+    email_msg.attach(msg)
+    email_msg = email_msg.as_string()
+
+    # send the message
+    print "Sending email message..."
+    try:
+        smtp = smtplib.SMTP('localhost')
+        smtp.set_debuglevel(1)
+        smtp.sendmail(sender, recipient, email_msg)
+    except Exception, e:
+        print "Error: %s" %str(e)
+    finally:
+        smtp.close()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='FTP Server Upload Example')
-    parser.add_argument('--ftp-server', action="store", dest="ftp_server", default=LOCAL_FTP_SERVER)
-    parser.add_argument('--file-name', action="store", dest="file_name", default=LOCAL_FILE)
-    # parser.add_argument('--username', action="store", dest="username", default=getpass.getuser())
+    parser = argparse.ArgumentParser(description='Email Example')
+    parser.add_argument('--sender', action="store", dest="sender",  default='XXXXX.com')
+    parser.add_argument('--recipient', action="store", dest="recipient")
     given_args = parser.parse_args()
-    ftp_server, file_name, username = given_args.ftp_server, given_args.file_name, "uftp"
-    password = getpass.getpass(prompt="Enter you FTP password: ")
-    ftp_upload(ftp_server, username, password, file_name)
+    email_dir_zipped(given_args.sender, given_args.recipient)
