f783096154449ed831e7cb1f8c01830ad10b39d3
557703988@139.com <557703988@139.com>
2017-03-24 02:30:04 -0700

    Chapter 05: check remote email via imap

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -4,27 +4,26 @@
 
 import argparse
 import getpass
-import poplib
+import imaplib
+GOOGLE_IMAP_SERVER = 'imap.googlemail.com'
+YIDONG_IMAP_SERVER = 'imap.139.com'
 
-# GOOGLE_POP3_SERVER = 'pop.googleemail.com'
-yidong_POP3_SERVER = 'pop.139.com'
-
-def download_email(username):
-    mailbox = poplib.POP3_SSL(yidong_POP3_SERVER, '995')
-    # mailbox = poplib.POP3_SSL(GOOGLE_POP3_SERVER, '995')
-    mailbox.user(username)
+def check_email(username):
+    mailbox = imaplib.IMAP4_SSL(YIDONG_IMAP_SERVER, '993')
     password = getpass.getpass(prompt="Enter your 139 password: ")
-    mailbox.pass_(password)
-    num_messages = len(mailbox.list()[1])
-    print "Total emails: %s" % num_messages
-    print "Getting last message"
-    for msg in mailbox.retr(num_messages)[1]:
-        print msg
-    mailbox.quit()
+    mailbox.login(username, password)
+    mailbox.select('Inbox')
+    typ, data = mailbox.search(None, 'ALL')
+    for num in data[0].split():
+        typ, data = mailbox.fetch(num, '(RFC822)')
+        print 'Message %s\n%s\n' % (num, data[0][1])
+        break
+    mailbox.close()
+    mailbox.logout()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Email Download Example')
     parser.add_argument('--username', action="store", dest="username", default=getpass.getuser())
     given_args = parser.parse_args()
     username = given_args.username
-    download_email(username)
+    check_email(username)
