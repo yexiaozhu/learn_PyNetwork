a610f014d5d71bd66b3d1e9efb63065c15b58a1e
557703988@139.com <557703988@139.com>
2017-03-24 01:47:15 -0700

    Chapter 05: upload file to ftp server

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,24 +2,32 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-FTP_SERVER_URL = 'ftp.debian.org'
-# FTP_SERVER_URL = 'ftp.kernel.org'
-
+import os
+import argparse
 import ftplib
+import getpass
 
-def test_ftp_connection(path, username, email):
-    # Open ftp connection
-    ftp = ftplib.FTP(path, username, email)
+LOCAL_FTP_SERVER = 'localhost'
+LOCAL_FILE = 'README.md'
 
-    # List the files in the /pub directory
-    # ftp.cwd("debian")
-    ftp.cwd("debian/tools")
-    # ftp.cwd("kernel/pub")
-    print "File list at %s:" %path
-    files = ftp.dir()
-    print files
-
-    ftp.quit()
+def ftp_upload(ftp_server, username, password, file_name):
+    print "Connecting to FTP server: %s" %ftp_server
+    ftp = ftplib.FTP(ftp_server)
+    print "Login to FTP server: user=%s" %username
+    ftp.login(username, password)
+    ext = os.path.splitext(file_name)[1]
+    if ext in (".txt", ".htm", ".html"):
+        ftp.storlines("STOR " + file_name, open(file_name))
+    else:
+        ftp.storbinary("STOR " +file_name, open(file_name, "rb"), 1024)
+    print "Uploaded file: %s" %file_name
 
 if __name__ == '__main__':
-    test_ftp_connection(path=FTP_SERVER_URL, username='anonymous', email='nobody@nourl.com')
+    parser = argparse.ArgumentParser(description='FTP Server Upload Example')
+    parser.add_argument('--ftp-server', action="store", dest="ftp_server", default=LOCAL_FTP_SERVER)
+    parser.add_argument('--file-name', action="store", dest="file_name", default=LOCAL_FILE)
+    # parser.add_argument('--username', action="store", dest="username", default=getpass.getuser())
+    given_args = parser.parse_args()
+    ftp_server, file_name, username = given_args.ftp_server, given_args.file_name, "uftp"
+    password = getpass.getpass(prompt="Enter you FTP password: ")
+    ftp_upload(ftp_server, username, password, file_name)
