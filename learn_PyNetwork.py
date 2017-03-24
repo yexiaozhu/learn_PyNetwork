ae19c7679c8025f90a5d696605f024683f51b2d8
557703988@139.com <557703988@139.com>
2017-03-24 02:25:04 -0700

    Chapter 05: download email via pop3

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,53 +2,29 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import os
 import argparse
-import smtplib
-import zipfile
-import tempfile
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
+import getpass
+import poplib
 
-print os.getcwd()
-def email_dir_zipped(sender, recipient):
-    zf = tempfile.TemporaryFile(prefix='mail', suffix='.zip')
-    zip = zipfile.ZipFile(zf, 'w')
-    print "Zipping current dir: %s" %os.getcwd()
-    for file_name in os.listdir(os.getcwd()):
-        zip.write(file_name)
-    zip.close()
-    zf.seek(0)
+# GOOGLE_POP3_SERVER = 'pop.googleemail.com'
+yidong_POP3_SERVER = 'pop.139.com'
 
-    # Create the message
-    print "Creating email message..."
-    email_msg = MIMEMultipart()
-    email_msg['Subject'] = 'File from path %s' %os.getcwd()
-    email_msg['To'] = ', '.join(recipient)
-    email_msg['From'] = sender
-    email_msg.preamble = 'Testing email from Python.\n'
-    msg = MIMEBase('application', 'zip')
-    msg.set_payload(zf.read())
-    encoders.encode_base64(msg)
-    msg.add_header('Content-Disposition', 'attachment', filename=os.getcwd()[-1] + '.zip')
-    email_msg.attach(msg)
-    email_msg = email_msg.as_string()
-
-    # send the message
-    print "Sending email message..."
-    try:
-        smtp = smtplib.SMTP('localhost')
-        smtp.set_debuglevel(1)
-        smtp.sendmail(sender, recipient, email_msg)
-    except Exception, e:
-        print "Error: %s" %str(e)
-    finally:
-        smtp.close()
+def download_email(username):
+    mailbox = poplib.POP3_SSL(yidong_POP3_SERVER, '995')
+    # mailbox = poplib.POP3_SSL(GOOGLE_POP3_SERVER, '995')
+    mailbox.user(username)
+    password = getpass.getpass(prompt="Enter your 139 password: ")
+    mailbox.pass_(password)
+    num_messages = len(mailbox.list()[1])
+    print "Total emails: %s" % num_messages
+    print "Getting last message"
+    for msg in mailbox.retr(num_messages)[1]:
+        print msg
+    mailbox.quit()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Email Example')
-    parser.add_argument('--sender', action="store", dest="sender",  default='XXXXX.com')
-    parser.add_argument('--recipient', action="store", dest="recipient")
+    parser = argparse.ArgumentParser(description='Email Download Example')
+    parser.add_argument('--username', action="store", dest="username", default=getpass.getuser())
     given_args = parser.parse_args()
-    email_dir_zipped(given_args.sender, given_args.recipient)
+    username = given_args.username
+    download_email(username)
