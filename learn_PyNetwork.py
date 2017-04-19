0dee0167e1f40245ef8c19bf5d08b42163be3927
557703988@139.com <557703988@139.com>
2017-04-19 00:23:02 -0700

    Chapter 06: google stock quote

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -3,74 +3,29 @@
 #author="yexiaozhu"
 
 import argparse
-import re
-import yaml
 import urllib
-import urllib2
+import re
+from datetime import datetime
 
-SEARCH_URL = 'https://%s.wikipedia.org/w/api.php?action=query' \
-             '&list=search&srsearch=%s&sroffset=%d&srlimit=%d&format=json'
+# SEARCH_URL = 'http://finance.google.com/finance?q='
+SEARCH_URL = 'https://gupiao.baidu.com/stock/us%s.html'
 
-class Wikipedia:
-
-    def __init__(self, lang='en'):
-        self.lang = lang
-
-    def _get_content(self, url):
-        request = urllib2.Request(url)
-        request.add_header('User-Agent', 'Mozilla/20.0')
-
-        try:
-            result = urllib2.urlopen(request)
-        except urllib2.HTTPError, e:
-            print "HTTP Error: %s" %(e.reason)
-        except Exception, e:
-            print "Error occured: %s" %str(e)
-        return result
-
-    def search_content(self, query, page=1, limit=10):
-        offset = (page - 1) * limit
-        # print 'offset:', type(offset)
-        # print 'limit:', limit
-        # print 'limit:', type(limit)
-        url = SEARCH_URL % (self.lang, urllib.quote_plus(query), offset, limit)
-        print url
-        content = self._get_content(url).read()
-        # print 'content:', content
-        # print 'content:', type(content)
-        content = content.replace('\t', '    ')
-        parsed = yaml.load(content)
-        # print parsed
-        search = parsed['query']['search']
-        # print search
-        if not search:
-            return
-
-        results = []
-        for article in search:
-            snippet = article['snippet']
-            snippet = re.sub(r'(?m)<.*?>', '', snippet)
-            snippet = re.sub(r'\s+', ' ', snippet)
-            snippet = snippet.replace(' . ', '. ')
-            snippet = snippet.replace(' , ', ', ')
-            snippet = snippet.strip()
-
-            results.append({
-                'title' : article['title'].strip(),
-                'snippet' : snippet
-            })
-
-        return results
+def get_quote(symbol):
+    # print SEARCH_URL + symbol
+    url = SEARCH_URL % symbol
+    print url
+    content = urllib.urlopen(url).read()
+    # print content
+    m = re.search('class="_close".*?>(.*?)<', content)
+    if m:
+        quote = m.group(1)
+    else:
+        quote = 'No quote available for: ' + symbol
+    return quote
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Wikipedia search')
-    parser.add_argument('--query', action="store", dest="query", required=True)
+    parser = argparse.ArgumentParser(description='Stock quote search')
+    parser.add_argument('--symbol', action="store", dest="symbol", required=True)
     given_args = parser.parse_args()
-    wikipedia = Wikipedia()
-    search_term = given_args.query
-    print "Searching Wikipedia for %s" % search_term
-    results = wikipedia.search_content(search_term)
-    print "Listing %s search results..." % len(results)
-    for result in results:
-        print "==%s== \n %s" %(result['title'], result['snippet'])
-    print "---- End of search results ----"
+    print "Searching stock quote for symbol '%s'" % given_args.symbol
+    print "Stock quote for %s at %s: %s" % (given_args.symbol, datetime.today(), get_quote(given_args.symbol))
