66dd2e9576dfbe8bbfd26ff052f68f8c9b5a8420
557703988@139.com <557703988@139.com>
2017-04-21 01:22:11 -0700

    Chapter 07: execute remote telnet cmd

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,65 +2,28 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import argparse
+import getpass
 import sys
-import httplib
-import re
+import telnetlib
 
-processed = []
+HOST = 'localhost'
 
-def search_links(url, depth, search):
-    # Process http links that are not processed yet
-    url_is_processed = (url in processed)
-    if (url.startswith("http://") and (not url_is_processed)):
-        processed.append(url)
-        url = host = url.replace("http://", "", 1)
-        path = "/"
+def run_telnet_session():
+    user = raw_input("Enter your remote account: ")
+    password = getpass.getpass()
 
-        urlparts = url.split("/")
-        print urlparts
-        if (len(urlparts) > 1):
-            host = urlparts[0]
-            path = url.replace(host, "", 1)
+    session = telnetlib.Telnet(HOST)
 
-        # Start crawing
-        print "Crawling URL path:%s%s " %(host, path)
-        conn = httplib.HTTPConnection(host)
-        req = conn.request("GET", path)
-        result = conn.getresponse()
-        print result
+    session.read_until("login: ")
+    session.write(user + '\n')
+    if password:
+        session.read_until("Password: ")
+        session.write(password + "\n")
 
-        # find the links
-        contents = result.read()
-        print result.status
-        print result.reason
-        print contents
-        all_links = re.findall('href="(.*?)"', contents)
-        print all_links
+    session.write("ls\n")
+    session.write("exit\n")
 
-        if (search in contents):
-            print "Found" + search + "at" + url
-
-        print "==> %s: processing %s links" %(str(depth), str(len(all_links)))
-        for href in all_links:
-            # Find relative urls
-            if (href.startswith("/")):
-                href = "http://" + host + href
-
-            # Recurse links
-            if (depth > 0):
-                search_links(href, depth-1, search)
-
-    else:
-        print "Skipping link: %s ..." %url
+    print session.read_all()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Webpage link crawler')
-    parser.add_argument('--url', action="store", dest="url", required=True)
-    parser.add_argument('--query', action="store", dest="query", required=True)
-    parser.add_argument('--depth', action="store", dest="depth", default=2)
-    given_args = parser.parse_args()
-    try:
-        search_links(given_args.url, given_args.depth, given_args.query)
-    except KeyboardInterrupt:
-        print "Aborting search by user request."
+    run_telnet_session()
