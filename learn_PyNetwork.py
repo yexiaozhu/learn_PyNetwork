670d0e1e10442b157098c630fc0748adb7fce032
557703988@139.com <557703988@139.com>
2017-04-21 02:26:51 -0700

    Chapter 07: print remote cpu info

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,35 +2,42 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import getpass
 import argparse
+import getpass
 import paramiko
 
-SOURCE = 'learn_PyNetwork.py'
-DESTINATION = '/home/yezi10/learn_PyNetwork.py'
+RECV_BYTES = 4096
+COMMAND = 'cat /proc/cpuinfo'
 
-def copy_file(hostname, port, username, password, src, dst):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    print "Connecting to %s \n with username=%s... \n" %(hostname, username)
-    print port
-    t = paramiko.Transport((hostname, port))
-    t.connect(username=username, password=password)
-    sftp = paramiko.SFTPClient.from_transport(t)
-    print "Copying file:%s to path: %s" %(src, dst)
-    sftp.put(src, dst)
-    sftp.close()
-    t.close()
+def print_remote_cpu_info(hostname, port, username, password):
+    client = paramiko.Transport((hostname, port))
+    client.connect(username=username, password=password)
+
+    stdout_data = []
+    stderr_data = []
+    session = client.open_channel(kind='session')
+    session.exec_command(COMMAND)
+    while True:
+        if session.recv_ready():
+            stdout_data.append(session.recv(RECV_BYTES))
+        if session.recv_stderr_ready():
+            stderr_data.append(session.recv_stderr(RECV_BYTES))
+        if session.exit_status_ready():
+            break
+
+    print 'exit status: ', session.recv_exit_status()
+    print ' '.join(stdout_data)
+    print ' '.join(stderr_data)
+
+    session.close()
+    client.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote file copy')
     parser.add_argument('--host', action="store", dest="host", default='localhost')
     parser.add_argument('--port', action="store", dest="port", default=22, type=int)
-    parser.add_argument('--src', action="store", dest="src", default=SOURCE)
-    parser.add_argument('--dst', action="store", dest="dst", default=DESTINATION)
     given_args = parser.parse_args()
     hostname, port = given_args.host, given_args.port
-    src, dst = given_args.src, given_args.dst
     username = raw_input("Enter the username:")
-    password = getpass.getpass("Enter password for %s: " % username)
-    copy_file(hostname, port, username, password, src, dst)
+    password = getpass.getpass("Enter password for %s: " %username)
+    print_remote_cpu_info(hostname, port, username, password)
