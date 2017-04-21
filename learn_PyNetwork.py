5be587ef813f514033841fbb8e234a5feb0efbc4
557703988@139.com <557703988@139.com>
2017-04-20 20:09:45 -0700

    Chapter 06: read bbc news feed

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -1,48 +1,28 @@
 #!/usr/bin/env python2.7.12
 #coding=utf-8
 #author="yexiaozhu"
-import base64
-import urllib2
 
-SEARCH_URL_BASE = 'https://api.github.com/repos'
+from datetime import datetime
+import feedparser
 
-import argparse
-import requests
-import json
+BBC_FEED_URL = 'http://feeds.bbci.co.uk/news/%s/rss.xml'
 
-def search_repository(author, repo, search_for='homepage'):
-    url = "%s/%s/%s" %(SEARCH_URL_BASE, author, repo)
-    token = "19c2cfeb60881c3640791584df8443472fb0615f"
-    password = "x-oauth-basic"
-    print "Searching Repo URL: %s" %url
-    result = urllib2.Request(url)
-    result.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (token, password)))
-    result.add_header("Content-Type", "application/json")
-    result.add_header("Accept", "application/json")
-    result = urllib2.urlopen(result)
-    result = result.read()
-    # print result
-    if(result):
-        repo_info = json.loads(result)
-        print "Github repository info for: %s" %repo
-        result = "No result found!"
-        keys = []
-        for key, value in repo_info.iteritems():
-            if search_for in key:
-                result = value
-        return result
+def read_news(feed_url):
+    try:
+        data = feedparser.parse(feed_url)
+    except Exception, e:
+        print "Got error: %s" %str(e)
+
+    for entry in data.entries:
+        print entry.title
+        print entry.link
+        print entry.description
+        print "\n"
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Github search')
-    parser.add_argument('--author', action="store", dest="author", required=True)
-    parser.add_argument('--repo', action="store", dest="repo", required=True)
-    parser.add_argument('--search_for', action="store", dest="search_for", required=True)
-    given_args = parser.parse_args()
-    result = search_repository(given_args.author, given_args.repo, given_args.search_for)
-
-    if isinstance(result, dict):
-        print "Got result for '%s'..." %(given_args.search_for)
-        for key,value in result.iteritems():
-            print "%s => %s" %(key,value)
-    else:
-        print "Got result for %s: %s" %(given_args.search_for, result)
+    print "==== Reading teachnology news feed from bbc.co.uk (%s)====" %datetime.today()
+    print "Enter the type of news feed: "
+    print "Available options are: world, uk, health, sci-tech, business, technology"
+    type = raw_input("News feed type:")
+    read_news(BBC_FEED_URL %type)
+    print "==== End of BBC news feed ===="
