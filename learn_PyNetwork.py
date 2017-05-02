281d6d9d111661dd63c54b6c483ea01003c1a2fe
557703988@139.com <557703988@139.com>
2017-05-02 04:01:39 -0700

    Chapter 08: get flickr photo info

--- learn_PyNetwork.py
+++ learn_PyNetwork.py
@@ -2,61 +2,76 @@
 #coding=utf-8
 #author="yexiaozhu"
 
-import xmlrpclib
 import argparse
-from base64 import b64decode
-from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+import json
+import requests
+import urllib2
+import flickrapi
+api_key = u'XXXXXX'
+api_secret = u'XXXXXX'
 
-class SecureXMLRPCServer(SimpleXMLRPCServer):
+try:
+    from local_settings import flickr_apikey
+except ImportError:
+    pass
 
-    def __init__(self, host, port, username, password, *args, **kargs):
-        self.username = username
-        self.password = password
-        # authenticate method is called from inner class
-        class VerifyingRequestHandler(SimpleXMLRPCRequestHandler):
-            # method to override
-            def parse_request(request):
-                if SimpleXMLRPCRequestHandler.parse_request(request):
-                    # authenticate
-                    if self.authenticate(request.headers):
-                        return True
-                    else:
-                        # if authentication fails return 401
-                        request.send_error(401, 'Authentication failed, Try again.')
-                return False
-        # initialize
-        SimpleXMLRPCServer.__init__(self, (host, port), requestHandler=VerifyingRequestHandler, *args, **kargs)
+def collect_photo_info(api_key, tag, max_count):
+    """Collects some interesting info about some photos from Flickr.com for a given tag"""
+    photo_collection = []
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
+    result_photo = flickr.photos.search(tags=tag)
+    result_photo = json.loads(result_photo)
+    count = 0
+    for p in result_photo['photos']['photo']:
+        if count >= max_count:
+            return photo_collection
+        print 'Processing photo: "%s"' %p['title']
+        photo = {}
+        info = flickr.photos.getInfo(photo_id=p['id'])
+        info = json.loads(info)
+        # url = "http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&photo_id= " + p['id'] + "&format=json&nojsoncallback=1&api_key=" + api_key
+        # info = requests.get(url).json()
+        photo["flickrid"] = p['id']
+        photo["title"] = info['photo']['title']['_content']
+        photo["description"] = info['photo']['description']['_content']
+        photo["page_url"] = info['photo']['urls']['url'][0]['_content']
+        photo["farm"] = info['photo']['farm']
+        photo["server"] = info['photo']['server']
+        photo["secret"] = info['photo']['secret']
 
-    def authenticate(self, headers):
-        headers = headers.get('Authorization').split()
-        basic, encoded = headers[0], headers[1]
-        if basic != 'Basic':
-            print 'Only basic anthentication supported'
-            return False
-        secret = b64decode(encoded).split(':')
-        username, password = secret[0], secret[1]
-        return True if (username==self.username and password==self.password) else False
-
-def run_server(host, port, username, password):
-    server = SecureXMLRPCServer(host, port, username, password)
-    # simple test function
-    def echo(msg):
-        """Reply client in uppser case"""
-        reply = msg.upper()
-        print "Client said: %s. So we echo that in uppercase: %s" % (msg, reply)
-        return reply
-    server.register_function(echo, 'echo')
-    print "Running a HTTP auth enabled XMLRPC server on %s:%s..." % (host, port)
-    server.serve_forever()
+        # comments
+        numcomments = int(json.loads(info['photo']['comments']['_content']))
+        if numcomments:
+            # print " Now reading comments (%d)..." % numcomments
+            # url = "http://api.flickr.com/services/rest/?method=flickr.photos.comments.getList&photo_id=" + p['id'] + "&format=json&nojsoncallback=1&api_key=" + api_key
+            # comments = requests.get(url).json()
+            comments = flickr.photos.comments.getList(photo_id=p['id'])
+            comments = json.loads(comments)
+            photo["comment"] = []
+            for c in comments['comments']['comment']:
+                comment = {}
+                comment["body"] = c['_content']
+                comment["authorid"] = c['author']
+                comment["authorname"] = c['authorname']
+                photo["comment"].append(comment)
+        photo_collection.append(photo)
+        count = count + 1
+    return photo_collection
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Multithreaded multicall XMLRPCServer / Proxy')
-    parser.add_argument('--host', action="store", dest="host", default='localhost')
-    parser.add_argument('--port', action="store", dest="port", default=8003, type=int)
-    parser.add_argument('--username', action="store", dest="username", default='user')
-    parser.add_argument('--password', action="store", dest="password", default='pass')
+    parser = argparse.ArgumentParser(description='Get photo info feom Flickr')
+    parser.add_argument('--api-key', action="store", dest="api_key", default=flickr_apikey)
+    parser.add_argument('--tag', action="store", dest="tag", default='sex')
+    parser.add_argument('--max-count', action="store", dest="max_count", default=10, type=int)
     # parse arguments
     given_args = parser.parse_args()
-    host, port = given_args.host, given_args.port
-    username, password = given_args.username, given_args.password
-    run_server(host, port, username, password)
+    api_key, tag, max_count = given_args.api_key, given_args.tag, given_args.max_count
+    photo_info = collect_photo_info(api_key, tag, max_count)
+    for photo in photo_info:
+        for k, v in photo.iteritems():
+            if k == "title":
+                print "Showiing photo info...."
+            elif k == "comment":
+                "\tPhoto got %s comments." % len(v)
+            else:
+                print "\t%s => %s" % (k, v)
